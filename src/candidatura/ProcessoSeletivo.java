dd429119b3a80eeaeb61ae5995d7b5abe3893363
Ryan <ryan.j.moore35@gmail.com>
2024-07-01 10:08:36 -0400

    aula 2

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -4,11 +4,10 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-    
+        
     }
     static void selecaoCandidatos() {
         String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO","MÔNICA","FABRÍCIO","MIRELA","DANIELA","JORGE"};
-        
         int candidatosSelecionados = 0;
         int candidatosAtual=0;
         double salarioBase=2000.0;
